2f776ea30fce6e6795cb1f9e05e3a62ccb181a74
Rahal-Kanishka <rahalncm@gmail.com>
2020-09-27 15:53:11 +0530

    added price calculation for multiple products

---
+++ src/main/java/com/store/demo/DTO/PriceDetailDTO.java
@@ -0,0 +1,59 @@
+package com.store.demo.DTO;
+
+public class PriceDetailDTO {
+
+    private int productId;
+    private int numberOfCartons;
+    private int totalUnits;
+    private double totalDiscount;
+    private String productName;
+    private double totalCost;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getNumberOfCartons() {
+        return numberOfCartons;
+    }
+
+    public void setNumberOfCartons(int numberOfCartons) {
+        this.numberOfCartons = numberOfCartons;
+    }
+
+    public int getTotalUnits() {
+        return totalUnits;
+    }
+
+    public void setTotalUnits(int totalUnits) {
+        this.totalUnits = totalUnits;
+    }
+
+    public double getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public void setTotalDiscount(double totalDiscount) {
+        this.totalDiscount = totalDiscount;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+}
